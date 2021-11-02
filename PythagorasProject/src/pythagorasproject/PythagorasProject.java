6aa230ebcde8ebd40d9ec4ed62024d86b4390274
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2021-11-02 10:03:09 +0700

    Add files via upload

---
+++ PythagorasProject/src/pythagorasproject/PythagorasProject.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pythagorasproject;
+import java.util.Scanner;
+/**
+ *
+ * @author ryanbrilianfatsena
+ */
+public class PythagorasProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Berikut ialah Gambar Segitiga Siku-Siku :");
+        System.out.println("      ___              ");
+        System.out.println("     |   |___          ");
+        System.out.println(" [a] |       |___  [c] ");
+        System.out.println("     |           |___  ");
+        System.out.println("     |_______________| ");
+        System.out.println("        [b]            ");
+        System.out.println("");
+        
+        System.out.println("Pilihan Menu Aplikasi Java Pythagoras Berikut :");
+        System.out.println("1. Cek triple pythagoras");
+        System.out.println("2. Menentukan sisi miring segitiga");
+        System.out.println("3. Menentukan sisi siku-siku segitiga");
+        System.out.println("4. Exit");
+        System.out.println("");
+        System.out.println("Silahkan Pilih Menu Pilihan Anda : ");
+        
+        Scanner pilihan = new Scanner(System.in);
+        
+        switch(pilihan.nextInt()){
+            case 1 :
+                triplePythagoras t = new triplePythagoras();
+                t.input();
+                t.cek();
+                break;
+            case 2 :
+                sisiMiring c = new sisiMiring();
+                c.input();
+                c.sisiMiring();
+                break;
+            case 3 :
+                siku2 siku = new siku2();
+                siku.input();
+                siku.sisi2();
+                break;
+            case 4:
+                System.out.println("exit...");
+                // Terminate JVM
+                System.exit(0);
+            default :
+                System.out.println("Silahkan Pilih Menu Pilihan Anda : ");
+                
+        }
+    }
+    
+}
